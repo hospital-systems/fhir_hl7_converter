d5ebd80257d3cf6ab4dd63f8ff1c04dec5f0d5cc
dmitry-n <nechayevdv@gmail.com>
2013-12-13 10:46:05 +0400

    tidied PatientAttributeConverter a bit

--- lib/fhir_hl7_converter/converters/patient_attribute_converter.rb
+++ lib/fhir_hl7_converter/converters/patient_attribute_converter.rb
@@ -74,40 +74,6 @@ module FhirHl7Converter
       hl7_to_nk1s(hl7).map{ |nk1| nk1_to_fhir_contact(nk1, terrminology) }
     end
 
-    def nk1_to_fhir_contact(nk1, terrminology)
-      Fhir::Patient::Contact.new(
-          relationships: [
-              Fhir::CodeableConcept.new(
-                  codings: [
-                      Fhir::Coding.new(
-                          system: 'http://hl7.org/fhir/patient-contact-relationship',
-                          code: nk1.relationship.identifier.to_p,
-                          display: nk1.relationship.identifier.to_p
-                      ),
-                      Fhir::Coding.new(
-                          system: 'http://hl7.org/fhir/patient-contact-relationship',
-                          code: nk1.contact_role.identifier.to_p,
-                          display: nk1.relationship.identifier.to_p
-                      )
-                  ],
-                  text: [nk1.relationship.identifier.to_p, nk1.contact_role.identifier.to_p].join(' ')
-              )
-          ],
-          name: DataTypeConverter.xpn_to_fhir_name(nk1.names.first),
-          telecoms: (
-          nk1.phone_numbers.map{ |xtn| DataTypeConverter.xtn_to_fhir_telecom(xtn, 'home') } +
-              nk1.business_phone_numbers.map{ |xtn| DataTypeConverter.xtn_to_fhir_telecom(xtn, 'work') }
-          ),
-          address: DataTypeConverter.xad_to_fhir_address(nk1.addresses.first),
-          gender: nk1_to_fhir_gender(nk1, terrminology),
-          organization: nil#, [Fhir::Organization]
-      )
-      #organizationNK1-13, NK1-30, NK1-31, NK1-32, NK1-41
-      #nk1.organization_name_nk1s
-      #nk1.contact_person_s_names
-      #nk1.contact_person_s_telephone_numbers
-      #nk1.contact_person_s_addresses
-    end
 
     def fhir_animal(hl7)
       hl7_to_pid(hl7).species_code
@@ -144,38 +110,11 @@ module FhirHl7Converter
       hl7_to_lans(hl7).map{ |lan| lan_to_fhir_communication(lan) } if hl7_to_lans(hl7)
     end
 
-    def lan_to_fhir_communication(lan)
-      lan.language_code
-      Fhir::CodeableConcept.new(
-          codings: [Fhir::Coding.new(
-                        system: 'TODO',
-                        code: 'TODO',
-                        display: 'TODO'
-                    )],
-          text: 'TODO'
-      )
-    end
 
     def fhir_links(hl7)
       hl7_to_pid(hl7).patient_identifier_lists
       hl7_to_mrg(hl7).try(:prior_patient_identifier_lists)
       nil
-    end
-
-    def administrative_sex_to_gender(administrative_sex, terrminology)
-      sex    = administrative_sex.try(:to_p)
-      coding = terrminology.coding(
-          'http://hl7.org/fhir/vs/administrative-gender',
-          sex
-      )
-      sex && Fhir::CodeableConcept.new(
-          codings: [Fhir::Coding.new(coding)],
-          text:    coding[:display] || sex
-      )
-    end
-
-    def nk1_to_fhir_gender(nk1, terrminology)
-      administrative_sex_to_gender(nk1.administrative_sex, terrminology)
     end
 
     def hl7_to_pid(hl7)
@@ -196,6 +135,69 @@ module FhirHl7Converter
 
     def hl7_to_mrg(hl7)
       hl7.mrg if hl7.respond_to?(:mrg)
+    end
+
+    def nk1_to_fhir_contact(nk1, terrminology)
+      Fhir::Patient::Contact.new(
+          relationships: [
+              Fhir::CodeableConcept.new(
+                  codings: [
+                      Fhir::Coding.new(
+                          system: 'http://hl7.org/fhir/patient-contact-relationship',
+                          code: nk1.relationship.identifier.to_p,
+                          display: nk1.relationship.identifier.to_p
+                      ),
+                      Fhir::Coding.new(
+                          system: 'http://hl7.org/fhir/patient-contact-relationship',
+                          code: nk1.contact_role.identifier.to_p,
+                          display: nk1.relationship.identifier.to_p
+                      )
+                  ],
+                  text: [nk1.relationship.identifier.to_p, nk1.contact_role.identifier.to_p].join(' ')
+              )
+          ],
+          name: DataTypeConverter.xpn_to_fhir_name(nk1.names.first),
+          telecoms: (
+          nk1.phone_numbers.map{ |xtn| DataTypeConverter.xtn_to_fhir_telecom(xtn, 'home') } +
+              nk1.business_phone_numbers.map{ |xtn| DataTypeConverter.xtn_to_fhir_telecom(xtn, 'work') }
+          ),
+          address: DataTypeConverter.xad_to_fhir_address(nk1.addresses.first),
+          gender: nk1_to_fhir_gender(nk1, terrminology),
+          organization: nil#, [Fhir::Organization]
+      )
+      #organizationNK1-13, NK1-30, NK1-31, NK1-32, NK1-41
+      #nk1.organization_name_nk1s
+      #nk1.contact_person_s_names
+      #nk1.contact_person_s_telephone_numbers
+      #nk1.contact_person_s_addresses
+    end
+
+    def nk1_to_fhir_gender(nk1, terrminology)
+      administrative_sex_to_gender(nk1.administrative_sex, terrminology)
+    end
+
+    def lan_to_fhir_communication(lan)
+      lan.language_code
+      Fhir::CodeableConcept.new(
+          codings: [Fhir::Coding.new(
+                        system: 'TODO',
+                        code: 'TODO',
+                        display: 'TODO'
+                    )],
+          text: 'TODO'
+      )
+    end
+
+    def administrative_sex_to_gender(administrative_sex, terrminology)
+      sex    = administrative_sex.try(:to_p)
+      coding = terrminology.coding(
+          'http://hl7.org/fhir/vs/administrative-gender',
+          sex
+      )
+      sex && Fhir::CodeableConcept.new(
+          codings: [Fhir::Coding.new(coding)],
+          text:    coding[:display] || sex
+      )
     end
   end
 end
