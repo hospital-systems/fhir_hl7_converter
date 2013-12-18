693b971e15b09bbb91955aeb89ac736d3a4e9b56
Maksym Bodnarchuk <bodnarchuk@gmail.com>
2013-12-18 13:30:03 +0200

    practitioner progress

--- lib/fhir_hl7_converter/converters/data_type_converter.rb
+++ lib/fhir_hl7_converter/converters/data_type_converter.rb
@@ -5,61 +5,61 @@ module FhirHl7Converter
     def xpn_to_fhir_name(xpn)
       families = xpn.family_name.surname.to_p
       givens = [
-          xpn.given_name,
-          xpn.second_and_further_given_names_or_initials_thereof
+        xpn.given_name,
+        xpn.second_and_further_given_names_or_initials_thereof
       ].compact.map(&:to_p).select{ |n| n.present? }.join(', ')
       prefixes = xpn.prefix.try(:to_p)
       suffixes = xpn.suffix.try(:to_p)
       Fhir::HumanName.new(
-          use: 'TODO',
-          text: [givens, families, prefixes, suffixes].join(' '),#FIXME
-          families: [families],
-          givens: [givens],
-          prefixes: [prefixes],
-          suffixes: [suffixes],
-          period: nil#Fhir::Period.new(start: DateTime.now, end: DateTime.now)
+        use: 'TODO',
+        text: [givens, families, prefixes, suffixes].join(' '),#FIXME
+        families: [families],
+        givens: [givens],
+        prefixes: [prefixes],
+        suffixes: [suffixes],
+        period: nil#Fhir::Period.new(start: DateTime.now, end: DateTime.now)
       )
     end
 
     def xtn_to_fhir_telecom(xtn, use)
       Fhir::Contact.new(
-          system: 'http://hl7.org/fhir/contact-system',
-          value: xtn.telephone_number.to_p,
-          use: use,
-          period: nil#Fhir::Period.new(start: DateTime.now, end: DateTime.now)
+        system: 'http://hl7.org/fhir/contact-system',
+        value: xtn.telephone_number.to_p,
+        use: use,
+        period: nil#Fhir::Period.new(start: DateTime.now, end: DateTime.now)
       )
     end
 
     def cx_to_fhir_identifier(cx)
       Fhir::Identifier.new(
-          use: 'usual',
-          key: cx.id_number.to_p,
-          label: cx.id_number.to_p,
-          system: cx.identifier_type_code.to_p,
-          period: nil,
-          assigner: nil#[Fhir::Organization]
+        use: 'usual',
+        key: cx.id_number.to_p,
+        label: cx.id_number.to_p,
+        system: cx.identifier_type_code.to_p,
+        period: nil,
+        assigner: nil#[Fhir::Organization]
       )
     end
 
     def xad_to_fhir_address(xad, terrminology)
       Fhir::Address.new(
-          use: address_type_to_use(xad.address_type.try(:to_p), terrminology),
-          text: [
-              xad.street_address.try(:street_or_mailing_address),
-              xad.street_address.try(:street_name),
-              xad.street_address.try(:dwelling_number),
-              xad.other_designation,
-              xad.city,
-              xad.state_or_province,
-              xad.zip_or_postal_code,
-              xad.country
-          ].map(&:to_p).join(' '),
+        use: address_type_to_use(xad.address_type.try(:to_p), terrminology),
+        text: [
+          xad.street_address.try(:street_or_mailing_address),
+          xad.street_address.try(:street_name),
+          xad.street_address.try(:dwelling_number),
+          xad.other_designation,
+          xad.city,
+          xad.state_or_province,
+          xad.zip_or_postal_code,
+          xad.country
+        ].map(&:to_p).join(' '),
           lines: [
-              xad.street_address.try(:street_or_mailing_address),
-              xad.street_address.try(:street_name),
-              xad.street_address.try(:dwelling_number),
-              xad.other_designation
-          ].map(&:to_p),
+            xad.street_address.try(:street_or_mailing_address),
+            xad.street_address.try(:street_name),
+            xad.street_address.try(:dwelling_number),
+            xad.other_designation
+        ].map(&:to_p),
           city: xad.city.to_p,
           state: xad.state_or_province.to_p,
           zip: xad.zip_or_postal_code.to_p,
@@ -69,10 +69,36 @@ module FhirHl7Converter
     end
 
     def xcn_to_fhir_practitioner(xcn)
+      families = xcn.family_name.surname.to_p
+      givens = [
+        xcn.given_name,
+        xcn.second_and_further_given_names_or_initials_thereof
+      ].compact.map(&:to_p).select{ |n| n.present? }.join(', ')
+      prefixes = xcn.prefix.try(:to_p)
+      suffixes = xcn.suffix.try(:to_p)
       Fhir::Practitioner.new(
-        text: Fhir::Narrative,
-        identifiers: Array[Fhir::Identifier],
-        name: Fhir::HumanName,
+        text: Fhir::Narrative.new(status: 'TODO', div: 'TODO'),
+        identifiers: [
+          Fhir::Identifier.new(
+            use: 'usual',
+            key: xcn.id_number.to_p,
+            label: xcn.id_number.to_p,
+            system: nil,
+            period: nil,
+            assigner: nil
+          )],
+            name: Fhir::HumanName.new(
+              use: 'TODO',
+              text: [givens, families, prefixes, suffixes].join(' '),#FIXME
+              families: [families],
+              givens: [givens],
+              prefixes: [prefixes],
+              suffixes: [suffixes],
+              period: nil#Fhir::Period.new(start: DateTime.now, end: DateTime.now)
+          )
+      )
+=begin
+    end
         telecoms: Array[Fhir::Contact],
         address: Fhir::Address,
         gender: Fhir::CodeableConcept,
@@ -91,6 +117,7 @@ module FhirHl7Converter
         ],
           communications: Array[Fhir::CodeableConcept]
       )
+=end
     end
 
     def ce_to_codeable_concept(ce)
@@ -117,9 +144,9 @@ module FhirHl7Converter
 
     def address_type_to_use(address_type, terrminology)
       terrminology.map_concept(
-          'http://hl7.org/fhir/v2/vs/0190',
-          address_type,
-          'http://hl7.org/fhir/vs/address-use'
+        'http://hl7.org/fhir/v2/vs/0190',
+        address_type,
+        'http://hl7.org/fhir/vs/address-use'
       )[:code]
     end
   end
--- spec/data_type_converter_spec.rb
+++ spec/data_type_converter_spec.rb
@@ -63,31 +63,94 @@ describe FhirHl7Converter::DataTypeConverter do
     end
   end
 
-  example do
-    puts pv1.attending_doctors.to_yaml#, Array[Xcn], position: "PV1.7", multiple: true
-    puts pv1.referring_doctors.to_yaml#, Array[Xcn], position: "PV1.8", multiple: true
-    puts pv1.consulting_doctors.to_yaml#, Array[Xcn], position: "PV1.9", multiple: true
-    puts pv1.admitting_doctors.to_yaml#, Array[Xcn], position: "PV1.17", multiple: true
-    pv1.attending_doctors.first.tap do |xcn|
-      subject.xcn_to_fhir_practitioner(xcn).tap do |p|
-        p.text.shoud# Fhir::Narrative
-        p.identifiers.should#, Array[Fhir::Identifier]
-        p.name.should#, Fhir::HumanName
-        p.telecoms.should#, Array[Fhir::Contact]
-        p.address.should#, Fhir::Address
-        p.gender.should#, Fhir::CodeableConcept
-        p.birth_date.should#, DateTime
-        p.photos.should#, Array[Fhir::Attachment]
-        p.organization.should#, [Fhir::Organization]
-        p.roles.should#, Array[Fhir::CodeableConcept]
-        p.specialties.should#, Array[Fhir::CodeableConcept]
-        p.period.should#, Fhir::Period
-        p.qualifications.first.tap do |q|#, Array[Qualification]
-          q.code.should#, Fhir::CodeableConcept
-          q.period.should#, Fhir::Period
-          q.issuer.should# [Fhir::Organization]
+  describe '#xcn_to_fhir_practitioner' do
+    it 'should create fhir practitioner from xcn segment' do
+      puts pv1.attending_doctors.to_yaml#, Array[Xcn], position: "PV1.7", multiple: true
+      puts pv1.referring_doctors.to_yaml#, Array[Xcn], position: "PV1.8", multiple: true
+      puts pv1.consulting_doctors.to_yaml#, Array[Xcn], position: "PV1.9", multiple: true
+      puts pv1.admitting_doctors.to_yaml#, Array[Xcn], position: "PV1.17", multiple: true
+      pv1.attending_doctors.first.tap do |xcn|
+        subject.xcn_to_fhir_practitioner(xcn).tap do |p|
+          #p.text.should# Fhir::Narrative
+          p.identifiers.first.key.should == xcn.id_number.to_p
+          p.name.tap do |n|
+            n.families.first.should == xcn.family_name.surname.to_p
+            n.givens.first.should == xcn.given_name.to_p + ', ' + xcn.second_and_further_given_names_or_initials_thereof.to_p
+            n.suffixes.first.should == xcn.suffix.to_p
+            n.prefixes.first.should == xcn.prefix.to_p
+          end
+          p.address.should#, Fhir::Address
+          p.gender.should#, Fhir::CodeableConcept
+          p.birth_date.should#, DateTime
+          p.photos.should#, Array[Fhir::Attachment]
+          p.organization.should#, [Fhir::Organization]
+          p.roles.should#, Array[Fhir::CodeableConcept]
+          p.specialties.should#, Array[Fhir::CodeableConcept]
+          p.period.should#, Fhir::Period
+          p.qualifications.first.tap do |q|#, Array[Qualification]
+            q.code.should#, Fhir::CodeableConcept
+            q.period.should#, Fhir::Period
+            q.issuer.should# [Fhir::Organization]
+          end
+          p.communications.should#, Array[Fhir::CodeableConcept]
         end
-        p.communications.should#, Array[Fhir::CodeableConcept]
+=begin
+class Xcn < ::HealthSeven::DataType
+  # Degree (e.g., MD)
+  attribute :degree, Is, position: "XCN.7"
+  # Source Table
+  attribute :source_table, Is, position: "XCN.8"
+  # Assigning Authority
+  attribute :assigning_authority, Hd, position: "XCN.9"
+  # Name Type Code
+  attribute :name_type_code, Id, position: "XCN.10"
+  # Identifier Check Digit
+  attribute :identifier_check_digit, St, position: "XCN.11"
+  # Check Digit Scheme
+  attribute :check_digit_scheme, Id, position: "XCN.12"
+  # Identifier Type Code
+  attribute :identifier_type_code, Id, position: "XCN.13"
+  # Assigning Facility
+  attribute :assigning_facility, Hd, position: "XCN.14"
+  # Name Representation Code
+  attribute :name_representation_code, Id, position: "XCN.15"
+  # Name Context
+  attribute :name_context, Ce, position: "XCN.16"
+  # Name Validity Range
+  attribute :name_validity_range, Dr, position: "XCN.17"
+  # Name Assembly Order
+  attribute :name_assembly_order, Id, position: "XCN.18"
+  # Effective Date
+  attribute :effective_date, Ts, position: "XCN.19"
+  # Expiration Date
+  attribute :expiration_date, Ts, position: "XCN.20"
+  # Professional Suffix
+  attribute :professional_suffix, St, position: "XCN.21"
+  # Assigning Jurisdiction
+  attribute :assigning_jurisdiction, Cwe, position: "XCN.22"
+  # Assigning Agency or Department
+  attribute :assigning_agency_or_department, Cwe, position: "XCN.23"
+end
+1. XCN – this table summarises the mappings from XCN to practitioner:
+7Degree (e.g., MD)MDPractitioner.qualification.code
+8Source TableNo equivalent in FHIR (e.g. extension if really necessary)
+9Assigning AuthorityPractitioner.identifier.system and/or Practitioner.name.assigner
+10Name Type CodeableConceptPractitioner.name.use
+11Identifier Check DigitNo equivalent in FHIR (e.g. extension if really necessary)
+12Check Digit SchemeNo equivalent in FHIR (e.g. extension if really necessary)
+13Identifier Type CodeableConceptPractitionerPractitioner.identifier.value
+14Assigning FacilityMaybe Practitioner.identifier.assigner
+15Name Representation CodeableConceptPractitionerPractitionerHelps to build Practitioner.name.text
+17PersonName Validity RangePractitioner.identifier.period
+18Name Assembly OrderHelps to build Practitioner.name.text
+19Effective DateTimePractitioner.identifier.period
+20Expiration DateTimePractitionerPractitioner.identifier.period
+21Professional SuffixPractitioner.qualification.code
+22Assigning JurisdictionPractitioner.qualification.issuer
+23Assigning Agency or DepartmentPractitioner.qualification.issuer
+24Security CheckNo equivalent in FHIR (e.g. extension if really necessary)
+25Security Check SchemeNoNo equivalent in FHIR (e.g. extension if really necessary)
+=end
       end
     end
   end
